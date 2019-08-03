24def6e5b5fcf760cd82d121a7fbdf3535dc73ba
Geremia Taglialatela <tagliala.dev@gmail.com>
2019-08-04 00:48:22 +0200

    Import Turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,11 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import Turbolinks from 'turbolinks'
+
 import 'src/application.scss'
 
 import 'src/bootstrap.js'
 import 'src/fontawesome.js'
 
 RailsUjs.start()
-Turbolinks.start()
